1699be031ce52a3b5e82f38ff6c3d2af030f20ee
kiyogit11 <y.kiyo75@gmail.com>
2026-06-11 01:08:40 +0900

    完了、未完了の実装完了

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-// $("#addTodo").click(function () {
-//   const inputTodo = $("input").val();
-//   $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
-//   $("input").val("");
-// });
+ $("#addTodo").click(function () {
+   const inputTodo = $("input").val();
+   $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+   $("input").val("");
+ });
 
-document.getElementById("addTodo").onclick=function(){
-    const inputTodo =document.querySelector("input").value;
+ $(document).on("change","input[type=checkbox]",function(){
+    if($(this).is(":checked")){
+        $(this).parent().css("text-decoration","line-through");
+    }else{
+        $(this).parent().css("text-decoration","none");
+    }
+ });
 
-    document.getElementById("todoList").insertAdjacentHTML("beforeend","<li><input type='checkbox'>" + inputTodo + "</li>");
-    document.querySelector("input").value="";
-}
+// document.getElementById("addTodo").onclick=function(){
+//     const inputTodo =document.querySelector("input").value;
+
+//     document.getElementById("todoList").insertAdjacentHTML("beforeend","<li><input type='checkbox'>" + inputTodo + "</li>");
+//     document.querySelector("input").value="";
+// }
